ca2fa6c942624209f6f086d4dd3a18cdde7edebc
Caro-CF <cfouquet.web@gmail.com>
2022-06-09 22:59:16 +0200

    add post userlist

--- pages/UserList.js
+++ pages/UserList.js
@@ -1,21 +1,46 @@
-function UserList({ users }) {
+function UserList({ data }) {
   return (
-    <ul>
-      {users.map((users) => (
-        <li>{users.firstname}</li>
-      ))}
-    </ul>
+    <>
+      <div>
+        <form
+          action="http://linksforcitizens.local:3000/api/users/"
+          method="post"
+        >
+          <label htlmfor={data.firstname}>First name:</label>
+          <input type="text" id="first" name="first" />
+          <label htlmfor={data.mail}>mail:</label>
+          <input type="text" id="mail" name="mail" />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+      
+      <ul>
+        {data.map((data) => (
+          <li key={data.id}>{data.firstname} &nbsp;{data.mail}</li>
+        ))}
+      </ul>
+      
+    </>
   );
 }
 
-export async function getStaticProps() {
-  const res = await fetch("http://linksforcitizens.local:3000/api/users");
-  const users = await res.json();
-  console.log("user list : " + users);
+// This gets called on every request
+export async function getServerSideProps() {
+  // Fetch data from external API
+  const res = await fetch(`http://linksforcitizens.local:3000/api/users/`);
+  const data = await res.json();
+//   const option = {
+//     method: "POST",
+//     headers: { "Content-Type": "application/json" },
+//     body: JSON.stringify({ data }),
+//   };
 
-  return {
-    props: { users },
-  };
+//   const response = await fetch(res, option);
+//   const responseJson = await response.json();
+//   console.log(responseJson);
+
+  // Pass data to the page via props
+  return { props: { data } };
 }
 
 export default UserList;
--- pages/login.js
+++ pages/login.js
@@ -1,7 +1,9 @@
 import Head from 'next/head'
 
-export default function Login() {
+function Login() {
     return(
         <head><title>Page de connexion à l&aposinterface de Ressources Citoyennes</title></head>
     )
 }
+
+export default Login
