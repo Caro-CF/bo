3d579fdc51a3586951310986869370df3d966710
Caro-CF <cfouquet.web@gmail.com>
2022-06-28 23:46:11 +0200

    function search in users

--- pages/users/index.jsx
+++ pages/users/index.jsx
@@ -11,7 +11,7 @@ const handleClose = () => setShow(false);
 const handleShow = () => setShow(true);
 function Index() {
   const [users, setUsers] = useState(null);
-// search
+  // search
   const [query, setQuery] = useState("");
 
   useEffect(() => {
@@ -41,15 +41,17 @@ function Index() {
     <div className="m-5">
       <h1>Utilisateurs</h1>
       {/* Search */}
-      <input
-        className="search"
-        placeholder="Search..."
-        onChange={(e) => setQuery(e.target.value.toLowerCase())}
-      />
+      
       <Link href="/users/add" className="btn btn-sm btn-success mb-2">
         Ajouter un utilisateur
       </Link>
-      
+
+      <input
+        className="search float-right"
+        placeholder="Rechercher..."
+        onChange={(e) => setQuery(e.target.value.toLowerCase())}
+      />
+
       <table className="table table-striped">
         <thead>
           <tr>
@@ -64,37 +66,45 @@ function Index() {
         </thead>
         <tbody>
           {users &&
-            users.filter((search) => search.firstname.toLowerCase().includes(query))
-            .map((user) => (
-              <tr key={user.usr_id}>
-                <td>
-                  <img
-                    src={`http://linksforcitizens.local:3000/public/upload/images/avatar/${user.avatar_img}`}
-                    width={50}
-                    height={50}
-                    alt="Avatar de l'utilisateur"
-                  />
-                </td>
-                <td>{user.pseudo}</td>
-                <td>{user.firstname}</td>
-                <td>{user.lastname}</td>
-                <td>{user.mail}</td>
-                <td>{user.roles}</td>
-                <td style={{ whiteSpace: "nowrap" }}>
-                  <Link
-                    href={`/users/edit/${user.usr_id}`}
-                    className="btn btn-sm btn-primary mr-1"
-                    id="btn-edit"
-                  >
-                    Edition
-                  </Link>
-                  <InfoModal
-                    element="utilisateur"
-                    id={user.usr_id}
-                    disabled="user"
-                  ></InfoModal>
+            users
+              .filter(
+                (search) =>
+                  search.firstname.toLowerCase().includes(query) ||
+                  search.lastname.toLowerCase().includes(query) ||
+                  search.roles.toLowerCase().includes(query) ||
+                  search.mail.toLowerCase().includes(query) ||
+                  search.pseudo.toLowerCase().includes(query) 
+              )
+              .map((user) => (
+                <tr key={user.usr_id}>
+                  <td>
+                    <img
+                      src={`http://linksforcitizens.local:3000/public/upload/images/avatar/${user.avatar_img}`}
+                      width={50}
+                      height={50}
+                      alt="Avatar de l'utilisateur"
+                    />
+                  </td>
+                  <td>{user.pseudo}</td>
+                  <td>{user.firstname}</td>
+                  <td>{user.lastname}</td>
+                  <td>{user.mail}</td>
+                  <td>{user.roles}</td>
+                  <td style={{ whiteSpace: "nowrap" }}>
+                    <Link
+                      href={`/users/edit/${user.usr_id}`}
+                      className="btn btn-sm btn-primary mr-1"
+                      id="btn-edit"
+                    >
+                      Edition
+                    </Link>
+                    <InfoModal
+                      element="utilisateur"
+                      id={user.usr_id}
+                      disabled="user"
+                    ></InfoModal>
 
-                  {/* <button
+                    {/* <button
                     onClick={() => deleteUser(user.usr_id)}
                     className="btn btn-sm btn-danger btn-delete-user"
                     disabled={user.isDeleting}
@@ -105,9 +115,9 @@ function Index() {
                       <span>Supprimer</span>
                     )}
                   </button> */}
-                </td>
-              </tr>
-            ))}
+                  </td>
+                </tr>
+              ))}
           {!users && (
             <tr>
               <td colSpan="4" className="text-center">
